a1d2f7d5c21e3dae4bb62df75ea74378bead90f7
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-05-28 10:02:25 -0700

    Added descriptions to libs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,10 @@
+/// Functions that mathematically generate mathematical constants
 pub mod constants;
+/// Functions related to factorial generation
 pub mod factorials;
+/// Functions related to perfect numbers
 pub mod perfect_numbers;
+/// Functions related to prime numbers
 pub mod primes;
+/// Functions that generate mathematical sequences
 pub mod sequences;
