278cce3fc198e61697bc06318a86831ec5aa8f14
Ben Puryear <54869170+Ben10164@users.noreply.github.com>
2024-08-06 12:41:45 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,6 @@ use bens_number_theory::sequences::*;
 use num::BigInt;
 
 fn main() {
-    let sequence: Vec<BigInt> = dying_rabbits_sequence(BigInt::from(5));
-    println!("{:?}", sequence);
-
-    println!("{:?}", golden_ratio(12_u8));
-    let test: num::rational::Ratio<BigInt> = estimate_pi_ratio(1);
+    let test: num::rational::Ratio<BigInt> = approx_sqrt(2, 50);
     println!("{:?}", test);
-    let t: i8 = 1_i8.pow(1);
 }
